c476f78b4e71d2ebd139e381ada37f645f0e5ba8
Arpita <surarpita01@gmail.com>
2023-04-02 23:07:17 +0530

    added hero section

--- App.js
+++ App.js
@@ -2,11 +2,15 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import NavBar from "./components/NavBar";
 import { useFonts } from "expo-font";
+import Hero from "./components/Hero";
 
 export default function App() {
   return (
-    <View style={styles.view}>
-      <NavBar />
+    <View style={{ backgroundColor: "#E5E5E5", width: "100%" }}>
+      <View style={styles.view}>
+        <NavBar />
+        <Hero />
+      </View>
     </View>
   );
 }
---
+++ components/Hero.js
@@ -0,0 +1,117 @@
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+} from "react-native";
+import { useFonts } from "expo-font";
+import AppText from "./AppText";
+export default function () {
+  const [fontsLoaded] = useFonts({
+    opensans_semibold: require("/assets/fonts/OpenSans-SemiBold.ttf"),
+    opensans_bold: require("/assets/fonts/OpenSans-Bold.ttf"),
+  });
+  return (
+    <View style={styles.hero}>
+      <View style={styles.message}>
+        <AppText style={styles.header}>
+          The essence of wellness and vitality in one place
+        </AppText>
+        <View style={styles.bulletList}>
+          <View style={styles.listItem}>
+            <Image
+              source={require("/assets/bullet.svg")}
+              style={styles.bullet}
+            />
+            <AppText style={{ fontSize: 16 }}>
+              Aromatherapeutic massages
+            </AppText>
+          </View>
+          <View style={styles.listItem}>
+            <Image
+              source={require("/assets/bullet.svg")}
+              style={styles.bullet}
+            />
+            <AppText style={{ fontSize: 16 }}>
+              Relaxation baths with the use of aromatherapy
+            </AppText>
+          </View>
+          <View style={styles.listItem}>
+            <Image
+              source={require("/assets/bullet.svg")}
+              style={styles.bullet}
+            />
+            <AppText style={{ fontSize: 16 }}>
+              SPA & wellness treatments
+            </AppText>
+          </View>
+
+          <TouchableOpacity style={styles.button}>
+            <AppText style={styles.buttonText}>Ready to relax?</AppText>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View>
+        <Image
+          source={require("/assets/main-img-preview.svg")}
+          style={styles.mainImg}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  hero: {
+    flexDirection: "row",
+    paddingHorizontal: 20,
+    justifyContent: "space-between",
+    paddingVertical: 20,
+  },
+  header: {
+    fontSize: 45,
+    opacity: "100%",
+    fontFamily: "opensans_bold",
+    lineHeight: 55,
+  },
+  message: {
+    gap: 10,
+    width: "45%",
+    marginTop: 26,
+    marginLeft: 30,
+  },
+  bulletList: {
+    gap: 10,
+  },
+  mainImg: {
+    height: 400,
+    width: 400,
+    borderRadius: 400 / 2,
+    marginRight: 20,
+  },
+  bullet: {
+    opacity: "100%",
+    height: 30,
+    width: 30,
+  },
+  listItem: {
+    flexDirection: "row",
+    gap: 15,
+    alignItems: "center",
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: "#954F25",
+    width: "max-content",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    opacity: "100%",
+  },
+});
